c4830e54142a7483a290e8f762f8193b092a5673
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-19 15:25:18 +0800

    Updates

---
+++ precache-manifest.03f3b43da8b4af623bf53172afca6e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a304c2263ca01678e1cd78f6e479efe",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "dcb22f4875e7e2960f8d",
+    "url": "/react-todo-list/static/css/main.3f48d989.chunk.css"
+  },
+  {
+    "revision": "88123760f8a053892fd8",
+    "url": "/react-todo-list/static/js/2.ddaa5437.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todo-list/static/js/2.ddaa5437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb22f4875e7e2960f8d",
+    "url": "/react-todo-list/static/js/main.44707c44.chunk.js"
+  },
+  {
+    "revision": "24763f4ac37c4c84a6e5",
+    "url": "/react-todo-list/static/js/runtime-main.b105f84a.js"
+  }
+]);
